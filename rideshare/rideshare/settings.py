1666306b1a128dc2087170fa4fc9bb6bba44fcdf
Adam Hinz <hinz.adam@gmail.com>
2012-05-05 16:01:41 +0000

    rework templates

--- rideshare/rideshare/settings.py
+++ rideshare/rideshare/settings.py
@@ -3,24 +3,13 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-import local_settings
+from local_settings import *
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
